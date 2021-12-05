08f8602e1b538c83178aecb1fe8c0ef9e18b1f40
Hoyt <hoytsreid@gmail.com>
2021-12-05 19:38:01 +0000

    :fire: Remove comment from inputReader

--- utils/inputReader.js
+++ utils/inputReader.js
@@ -1,6 +1,5 @@
 const fileSync = require('fs');
 
-// Reads an 'input.txt' file and returns an array of strings
  exports.read = function(path) {
   const input = fileSync.readFileSync(path, 'utf-8');
   return input.split('\n');
