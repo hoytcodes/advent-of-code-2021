eb8e12381811a2d254103bc1dcf314db02242bdb
Hoyt <hoytsreid@gmail.com>
2021-12-05 22:07:58 +0000

    :sparkles: Add reader for bingo input

--- utils/inputReader.js
+++ utils/inputReader.js
@@ -9,3 +9,10 @@ exports.readNumerical = function(path) {
   const input = this.read(path);
   return input.map(entry => parseInt(entry, 10));
 }
+
+exports.readBingo = function(path) {
+  const input = fileSync.readFileSync(path, 'utf-8');
+  let bingoCards = input.split('\n\n');
+  const numberDraws = bingoCards.shift().split(',').map(number => parseInt(number, 10));
+  return [numberDraws, bingoCards];
+}
