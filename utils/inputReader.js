d83d07fe37a668bd30efe8c2046c9f16bac53ea7
Hoyt <hoytsreid@gmail.com>
2021-12-07 23:55:51 +0000

    :sparkles: Add reader for numerical arrays

--- utils/inputReader.js
+++ utils/inputReader.js
@@ -16,3 +16,9 @@ exports.readBingo = function(path) {
   const numberDraws = bingoCards.shift().split(',').map(number => parseInt(number, 10));
   return [numberDraws, bingoCards];
 }
+
+exports.readNumericalArray = function(path) {
+  const input = fileSync.readFileSync(path, 'utf-8').trim();
+  const inputArray = input.split(',');
+  return inputArray.map(entry => parseInt(entry, 10));
+}
